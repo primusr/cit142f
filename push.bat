79309222bc9269545add87f4e815fbe2a5ba200e
primusr@M307.UC-BCF.EDU.PH <primusr@M307.UC-BCF.EDU.PH>
2022-09-14 17:46:38 +0800

    -batch upload

--- push.bat
+++ push.bat
@@ -1,5 +1,5 @@
 @echo off
-git remote set-url origin https://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@github.com/primusr/cit142f.git
+git remote set-url origin https://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@github.com/primusr/cit142f.git
 pause
 git add .
 git commit -m -"batch upload"
